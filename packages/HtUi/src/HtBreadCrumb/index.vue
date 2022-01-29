193c1a8ee00b049a1105ae2ba6a31ace6a9423bb
zourong <zourong@126.com>
2022-01-29 15:36:51 +0800

    feat: 面包屑组件添加

---
+++ packages/HtUi/src/HtBreadCrumb/index.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="ht-bread-crumb">
+    <div
+      v-for="(item, index) in data.list"
+      :key="index"
+      class="item"
+      @click="onItemClick(item, index)"
+    >
+      <span class="separator" v-if="index !== 0">{{
+        data.separator || defaultSeparator
+      }}</span>
+      <span
+        :class="['name', { 'name-curr': item.isActive }]"
+        :style="`color: ${item.isActive ? data.activeColor : data.color}`"
+      >
+        <!-- 默认内容插槽 -->
+        <slot :scope="data">
+          {{ item.name }}
+        </slot>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import { BreadCrumbData, BreadCrumbItem } from "./types";
+
+/**
+ * 项目中使用到的面包屑。
+ * */
+export default defineComponent({
+  name: "HtBreadCrumb",
+
+  props: {
+    data: {
+      type: Object as PropType<BreadCrumbData>,
+      required: true,
+      default: () => ({
+        list: [],
+        separator: "/",
+      }),
+    },
+  },
+
+  setup(props, { emit }) {
+    const defaultSeparator = "/";
+
+    /**
+     * 面包屑点击事件
+     * @param {Object} item 点击的面包屑item对象
+     * @param {Number} index 点击的面包屑索引值
+     * @returns void
+     */
+    const onItemClick = (item: BreadCrumbItem, index: number) => {
+      /**
+       * 面包屑点击事件触发
+       * @param {Object} item 点击的面包屑item对象
+       * @param {Number} index 点击的面包屑索引值
+       */
+      emit("on-click", item, index);
+    };
+
+    return {
+      defaultSeparator,
+      onItemClick,
+    };
+  },
+});
+</script>
+
+<style lang="less" scoped>
+@import "./index.less";
+</style>
---
+++ packages/HtUi/src/HtBreadCrumb/types.ts
@@ -0,0 +1,17 @@
+import { PublicProps } from "../types";
+
+export interface BreadCrumbItem {
+  name: string; // 面包屑名称
+  isActive?: boolean; // 当前面包屑是否选中
+}
+
+export interface BreadCrumbData {
+  list: BreadCrumbItem[]; // 面包屑列表
+  separator?: string; // 面包屑分隔符
+  color?: string; // 面包屑默认字体颜色
+  activeColor?: string; // 当前选中面包屑文字颜色
+}
+
+export type Props = PublicProps<{
+  data: BreadCrumbData;
+}>;
