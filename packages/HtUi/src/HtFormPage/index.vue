25d199702033a5082035eb5b4ed008a9e7179926
zourong <zourong@126.com>
2022-07-19 20:13:29 +0800

    feat: 方法更新

--- packages/HtUi/src/HtFormPage/index.vue
+++ packages/HtUi/src/HtFormPage/index.vue
@@ -513,6 +513,7 @@ export default defineComponent({
       formModel,
       formConfig,
       formGroup,
+      onInitFormModel,
       onGetFormItemConfig,
       formActions,
       onGetFormInitInfo,
